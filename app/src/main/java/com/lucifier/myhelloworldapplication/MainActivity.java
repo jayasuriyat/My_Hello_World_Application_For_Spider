2c6756c46bd208e8c40c6e8ebaf69e64335e681f
jayasuriyat <eevxlm@gmail.com>
2015-05-20 22:20:56 +0530

    Initial commit

---
+++ app/src/main/java/com/lucifier/myhelloworldapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package com.lucifier.myhelloworldapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.view.View.OnClickListener;
+
+
+public class MainActivity extends ActionBarActivity {
+    int i=0;
+    Button button;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.button);
+        textView=(TextView)findViewById(R.id.textView);
+        button.setOnClickListener(o);
+        textView.setOnClickListener(o);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    OnClickListener o=new OnClickListener()
+    {
+        @Override
+        public void onClick(View v) {
+            i++;
+            textView.setText("Hello World!");
+
+        }
+    };
+}
